7b85ed7bbcc820cbd13095ddf2f9b3f6a1d3c255
John Warren <johnv8@hotmail.co.uk>
2022-07-03 00:28:10 +0100

    link nav list

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,14 @@ function Header() {
           <h1>INSIDE</h1>
         </div>
         <nav className="links" >
-        <i className="fa-brands fa-github-square"></i>
-        <i className="fa-brands fa-linkedin"></i>
+        <a href="https://github.com/johnw-north">
+          <i className="fa-brands fa-github-square"></i>
+        </a>
+        <a href="https://linkedin.com/in/john-warren-north">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
         <i class="fa-solid fa-address-card"></i>
+        <i className="fa-solid fa-at"></i>
         </nav>     
       </div>
     );
