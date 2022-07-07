f072669b5ebb89dadb07eb0de80224097ed50632
John Warren <johnv8@hotmail.co.uk>
2022-07-07 16:54:15 +0100

    add my about

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ function About() {
         <div className="about__bio">
           <h1 className="bio__title">ABOUT ME</h1>
           <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            I have been programming for 2 years, starting in the virtual reality industry and now expanding my knowledge into web development. My focus is backend development, creating APIs, and working with databases. From having a background in management, I understand how producing organised and clean code is important.
           </p>
         </div>
       </div>
