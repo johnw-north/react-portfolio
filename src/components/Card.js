ea697fcf23bf11eb05f33f8b470aa864a7f4bcd3
John Warren <johnv8@hotmail.co.uk>
2022-07-22 10:25:39 +0100

    add petrol pal project

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,9 @@ function Card(props) {
     return (
       <div className="card">
         <h1 className="card__title">{props.title}</h1>
-        <img src={props.image} alt="Project" />
+        <a href={props.website}>
+          <img src={props.image} alt="Project" />
+        </a>
         <p>{props.description}</p>
         <div className="card__links">
           <a href={props.github}>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import Card from "./Card";
+import PetrolPal from "../images/PetrolPal.png"
 import profilePic from '../images/pill.png';
 
 function Projects() {
@@ -7,11 +8,11 @@ function Projects() {
         <h1 className="projects__title">Projects</h1>
         <div className="projects__container">
           <Card 
-            title="Petrol Pal (WIP)"
-            image={profilePic}
+            title="Petrol Pal"
+            image={PetrolPal}
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            github=""
-            website=""
+            github="https://github.com/johnw-north/petrol-pal"
+            website="https://johnw-north.github.io/petrol-pal/"
             />
           <Card 
             title="WIP"
